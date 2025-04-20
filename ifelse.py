f665d0a7f2be89e2731ab64aacd6beb82a97dd6d
Prajwal Vijay Ankushrao <prajwal11@Prajwals-MacBook-Air.local>
2025-04-21 00:38:17 +0530

    Basic Q2

---
+++ ifelse.py
@@ -0,0 +1,27 @@
+#Given marks of a student, print on the screen:
+
+#Grade A if marks >= 90
+#Grade B if marks >= 70
+#Grade C if marks >= 50
+#Grade D if marks >= 35
+#Fail, otherwise.
+
+
+marks = int(input("Enter marks to know the grade: "))
+
+def getGrade(marks):
+    if marks >= 90:
+        return "Grade A"
+    elif marks >= 70:
+        return "Grade B"
+    elif marks >= 50:
+        return "Grade C"
+    elif marks >= 35:
+        return "Grade D"
+    else:
+        return "Fail"
+
+# Call the function and print the grade
+grade = getGrade(marks)
+print(f"Your grade is: {grade}")
+
