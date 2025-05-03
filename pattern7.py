61f02cc1235ac45905a09d873ceda32218a632c3
Prajwal Vijay Ankushrao <prajwal11@Prajwals-MacBook-Air.local>
2025-05-03 13:18:13 +0530

    Half Diamond Star Pattern

---
+++ pattern7.py
@@ -0,0 +1,32 @@
+# Examples:
+
+# Input Format: N = 3
+# Result: 
+#   *  
+#   **
+#   ***  
+#   **
+#   *   
+# Input Format: N = 6
+# Result:   
+#      *
+#      **
+#      *** 
+#      ****
+#      *****
+#      ******  
+#      *****
+#      ****
+#      ***    
+#      **
+#      *
+n = int(input("enter n"))
+for i in range(1,2*n):
+    if i<=n:
+        for j in range(i):
+            print("*",end="")
+        print()
+    elif i>n:
+        for j in range((2*n)-i):
+            print("*",end="")
+        print()
